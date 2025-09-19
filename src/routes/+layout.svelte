15768235a95b33e1c6d6bcdfbcc57d8bd9f62fce
PXR <pxr2709@gmail.com>
2025-09-19 21:09:29 +0700

    favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,10 @@
 <script lang="ts">
   import "../app.css";
-  import favicon from "$lib/assets/favicon.ico";
   import * as Sidebar from "$lib/components/ui/sidebar";
   import { ThemeSidebar } from "$lib/components";
 
   let { children } = $props();
 </script>
-
-<svelte:head>
-  <link rel="icon" href={favicon} />
-</svelte:head>
 
 <Sidebar.Provider class="bg-background flex-row-reverse">
   <Sidebar.Root side="right" class="">
